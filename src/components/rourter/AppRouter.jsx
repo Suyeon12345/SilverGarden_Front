3edc9889ed1cf48e1c49017e636c91329b2314b2
jeeyeon <jeeyeon118@gmail.com>
2024-02-07 18:02:30 +0900

    program.jsx 생성_지연_240207

--- src/components/rourter/AppRouter.jsx
+++ src/components/rourter/AppRouter.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import Home from '../../pages/home/Home'
 import Approval from '../../pages/approval/Approval'
 import Notice from '../../pages/notice/Notice'
+import Program from '../../pages/program/Program'
 
 const AppRouter = () => {
   return (
@@ -10,6 +11,7 @@ const AppRouter = () => {
     <Route path="/" exact={true} element={<Home/>}></Route>
     <Route path="/approval" exact={true} element={<Approval/>}></Route>
     <Route path="/notice" exact={true} element={<Notice/>}></Route>
+    <Route path="/program" exact={true} element={<Program/>}></Route>
   </Routes>
   )
 }
---
+++ src/pages/program/Program.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import styles from './programhome.module.css'
+import SidebarCommon from '../../components/sidebar/SidebarCommon';
+import { faComment, faCrosshairs, faSolarPanel } from '@fortawesome/free-solid-svg-icons';
+
+
+const Program = () => {
+    const list = [//이 리스트를 props를 넣어주면 원하는 목록의 사이드바를 생성 가능
+    {
+    label: '결재',//목록이름
+    icon: faSolarPanel,//fontAwsome 아이콘 명
+    isOpen:true,
+    subMenuItems: [//서브목록 정보
+        { label: '결재대기함', icon: faCrosshairs},//서브목록이름, 아이콘명, 클릭시넘어갈 url
+        { label: '결재진행함', icon: faComment},
+        { label: '반려문서함', icon: faComment},
+        { label: '결재완료문서', icon: faComment},
+    ],
+    },
+    {
+    label: '기안',
+    icon: faSolarPanel,
+    isOpen:true,
+    subMenuItems: [
+        { label: '결재문서작성', icon: faComment },
+        { label: '임시보관함', icon: faComment },
+        { label: '결재요청함', icon: faComment },
+    ],
+    },
+    ];
+    return (
+        <div className={styles.programWrap}>
+            <div className={styles.programSidebarWrap}><SidebarCommon list={list}/></div>
+            <div className={styles.programContentWrap}>Content</div>
+        </div>
+    )
+}
+
+export default Program
