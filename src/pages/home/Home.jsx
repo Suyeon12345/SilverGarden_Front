2d9c6bb903ca6ef53f527555a945037d0cc93106
jeeyeon <jeeyeon118@gmail.com>
2024-02-09 19:01:02 +0900

    Revert "Merge branch 'dev' of https://github.com/Suyeon12345/SilverGarden_Front into program"
    This reverts commit a1e0d1d59447184d5664fa70c94435ffe994ce19, reversing
    changes made to 71ca4cebdf715805c0ed8062971965f6779005e2.

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,5 @@
-import {faList, faPenNib, faPenToSquare,} from '@fortawesome/free-solid-svg-icons';
-import React, { useState } from 'react'
+import {faPenNib,} from '@fortawesome/free-solid-svg-icons';
+import React from 'react'
 import styles from './home.module.css'
 import SidebarCommon from '../../components/sidebar/SidebarCommon';
 import HomeProfile from './HomeProfile';
@@ -8,19 +8,12 @@ const Home = () => {
     const sidebarList = [{
       label: '홈',
       icon: faPenNib,
-      isOpen:true,//시작시 열려있도록 함
-      subMenuItems: [
-        { label: '전체공지', icon: faList},
-        { label: '공지작성', icon: faPenToSquare },
-      ],
+      isOpen:true,
     },
   ];
-  const handleMenu = (menuTitle) =>{//사이드바 메뉴를 클릭했들 때 해당 페이지를 렌더링하기 위해 함수를 선언
-    setPage(menuTitle);
-  } 
+  const handleSidebarMenu = () =>{//사이드바 메뉴를 클릭했들 때 해당 페이지를 렌더링하기 위해 함수를 선언
 
-  const [page, setPage] = useState("전체공지");
-  console.log(page);
+  } 
 
   return (
     <div className={styles.homeWrap}>
